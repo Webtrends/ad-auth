0dd878542e476b84359f5a9ac8a9777fe55ac8aa
Tim Smith <tsmith@llnw.com>
2013-11-17 20:04:26 -0800

    Add oracle to the supported OS list and update my contact info

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'ad-auth'
 maintainer       'Peter Crossley, Tim Smith'
-maintainer_email 'peterc@xley.com, tim.smith@webtrends.com'
+maintainer_email 'peterc@xley.com, tsmith@llnw.com'
 license          'Apache 2.0'
 description      'Configures Active Directory authentication support using Likewise Open 6'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
@@ -8,6 +8,6 @@ version          '1.0.7'
 
 depends          'sudo'
 
-%w{ redhat centos fedora ubuntu debian amazon scientific }.each do |os|
+%w{ redhat centos fedora ubuntu debian amazon scientific oracle }.each do |os|
   supports os
 end
